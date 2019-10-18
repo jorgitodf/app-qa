0c13a8a249710151b999264af056264dbb9bcdfd
jorgitodf <jspaiva.1977@gmail.com>
2019-10-17 21:40:39 -0300

    Registrando novo usuário

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,46 @@
+const bcrypt = require('bcrypt-nodejs');
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        name: {
+            type: DataTypes.STRING,
+            defaultValue: ''
+        },
+        age: {
+            type: DataTypes.INTEGER
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: ''
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        first_access: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
+    });
+
+    User.prototype.validatePassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+    User.addHook('beforeCreate', function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     });
 
     return User;
--- src/routes/user/user-post.js
+++ src/routes/user/user-post.js
@@ -1,5 +1,13 @@
 const User = require('./../../models/index').User;
 
-    module.exports = (req, res) => {
-
+module.exports = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        if (!user) {
+            return res.redirect('/404');
+        }
+        return res.redirect('/');
+    } catch(err) {
+        console.log('ERROR', err);
+    }
 }
